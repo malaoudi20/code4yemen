fe9c2fc930f006c2572d35231dad30d1bde35317
sondosAlansi <sondos.alansi@yahoo.com>
2020-04-05 15:52:33 +0300

    edited reponsivness

--- ar.php
+++ ar.php
@@ -488,7 +488,7 @@
         <div class="bootstrap-wrapper">
           <div class="container">
             <p>ادعم الشباب اليمني  ليصبحوا قادة المستقبل في المجال التقني</p>
-            <a href="#" class="main-btn">تبرع</a>
+            <script type="text/javascript" defer src="https://donorbox.org/install-popup-button.js"></script><a class="dbox-donation-button" style="background: #C86567 url(https://d1iczxrky3cnb2.cloudfront.net/red_logo.png) no-repeat 37px;color: #fff;text-decoration: none;font-family: Verdana,sans-serif;display: inline-block;font-size: 16px;padding: 15px 38px;padding-left: 75px;-webkit-border-radius: 2px;-moz-border-radius: 2px;border-radius: 2px;box-shadow: 0 1px 0 0 #1f5a89;text-shadow: 0 1px rgba(0, 0, 0, 0.3);" href="https://donorbox.org/donate-1-5?default_interval=o">Donate</a>
           </div>
         </div>
        
